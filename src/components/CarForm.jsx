42fc7e215267f830ec4a6ab8c741c3806e2d97ac
AliAK30 <aliahmed.k30062@gmail.com>
2024-06-08 21:57:47 +0500

    changes

---
+++ src/components/CarForm.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import getCookie from "../utils/getCookie";
+import axios from "axios";
+
+export default CarForm = async ({fullname}) => {
+    
+
+    return(
+        <>
+        <form>
+
+        </form>
+        </>
+    )
+
+}
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import { useState } from "react";
 import getCookie from "../utils/getCookie";
 import axios from "axios";
-
+import CarForm from "./CarForm";
 
 export default function SignIn() {
-
   const [token, setToken] = useState(getCookie("carent-session-token"));
+  const [fullname, setFullname] = useState("");
 
-  //setToken(getCookie("carent-session-token"))
   
   const signIn = async (e) => {
-
     e.preventDefault();
 
     const obj = {
@@ -20,9 +18,10 @@ export default function SignIn() {
     };
 
     axios
-      .post("http://localhost:8080/auth/signin", obj, { withCredentials: true },)
+      .post("http://localhost:8080/auth/signin", obj, { withCredentials: true })
       .then(function (response) {
-        setToken(getCookie("carent-session-token"))
+        setToken(getCookie("carent-session-token"));
+        setFullname(response.data.fullname);
       })
       .catch(function (error) {
         console.log(error);
@@ -31,25 +30,33 @@ export default function SignIn() {
 
   const signOut = () => {
     //e.preventDefault()
-    axios.get("http://localhost:8080/auth/signout", { withCredentials: true },).then((res => {
-      setToken(getCookie("carent-session-token"))
-    }))
-  }
+    axios
+      .get("http://localhost:8080/auth/signout", { withCredentials: true })
+      .then((res) => {
+        setToken(getCookie("carent-session-token"));
+      });
+  };
 
   return (
     <>
-    {token === "" ? <form
-      onSubmit={(e) => {
-        signIn(e);
-      }}
-    >
-      <label>Email address</label>
-      <input type="email" id="email" placeholder="Enter email" />
+      {token === "" ? (
+        <form
+          onSubmit={(e) => {
+            signIn(e);
+          }}
+        >
+          <label>Email address</label>
+          <input type="email" id="email" placeholder="Enter email" />
 
-      <label>Password</label>
-      <input type="password" id="password" placeholder="Password" />
-      <button type="submit">Sign In</button>
-    </form> : <button onClick={signOut}>Signout</button>}
+          <label>Password</label>
+          <input type="password" id="password" placeholder="Password" />
+          <button type="submit">Sign In</button>
+        </form>
+      ) : (
+        <div>
+          <CarForm fullname={fullname} /> <button onClick={signOut}>Signout</button>
+        </div>
+      )}
     </>
   );
 }
