dd23e30eae9524223439faa070d24e109ebfb39d
Waleed Ahmed <ateeq800@gmail.com>
2024-06-12 04:27:43 +0500

    year, cities, category changed

--- src/utils/carFormHelper.js
+++ src/utils/carFormHelper.js
@@ -7,6 +7,7 @@ export const categories = [
     "Hatchback",
     "Coupe",
     "Crossover",
+    "Luxury",
 ];
 
 const getYears = (startYear) => {
@@ -20,37 +21,14 @@ const getYears = (startYear) => {
 }
 
 export const years = [
-    '1998', '1999', '2000', '2001',
-    '2002', '2003', '2004', '2005',
-    '2006', '2007', '2008', '2009',
-    '2010', '2011', '2012', '2013',
-    '2014', '2015', '2016', '2017',
-    '2018', '2019', '2020', '2021',
-    '2022', '2023', '2024',
+    
+    '2024', '2023', '2022',  '2021',
+    '2020', '2019', '2018',  '2017',
+    '2016', '2015', '2014',  '2013',
+    '2012', '2011', '2010',  '2009',
   ];
 
-export const cities = ["Islamabad", "Ahmed Nager", "Ahmadpur East", "Ali Khan", "Alipur", "Arifwala", "Attock", "Bhera",
-     "Bhalwal", "Bahawalnagar", "Bahawalpur", "Bhakkar", "Burewala", "Chillianwala", "Chakwal", "Chichawatni", "Chiniot",
-      "Chishtian", "Daska", "Darya Khan", "Dera Ghazi", "Dhaular", "Dina", "Dinga", "Dipalpur", "Faisalabad", 
-      "Fateh Jhang", "Ghakhar Mandi", "Gojra", "Gujranwala", "Gujrat", "Gujar Khan", "Hafizabad", "Haroonabad", "Hasilpur", 
-      "Haveli", "Lakha", "Jalalpur", "Jattan", "Jampur", "Jaranwala", "Jhang", "Jhelum", "Kalabagh", "Karor Lal", "Kasur", 
-      "Kamalia", "Kamoke", "Khanewal", "Khanpur", "Kharian", "Khushab", "Kot Adu", "Jauharabad", "Lahore", "Lalamusa", "Layyah", 
-      "Liaquat Pur", "Lodhran", "Malakwal", "Mamoori", "Mailsi", "Mandi Bahauddin", "mian Channu", "Mianwali", "Multan", "Murree", 
-      "Muridke", "Mianwali Bangla", "Muzaffargarh", "Narowal", "Okara", "Renala Khurd", "Pakpattan", "Pattoki", "Pir Mahal", "Qaimpur", 
-      "Qila Didar", "Rabwah", "Raiwind", "Rajanpur", "Rahim Yar", "Rawalpindi", "Sadiqabad", "Safdarabad", "Sahiwal", "Sangla Hill", "Sarai Alamgir", 
-      "Sargodha", "Shakargarh", "Sheikhupura", "Sialkot", "Sohawa", "Soianwala", "Siranwali", "Talagang", "Taxila", "Toba Tek", "Vehari", "Wah Cantonment", 
-      "Wazirabad", "Badin", "Bhirkan", "Rajo Khanani", "Chak", "Dadu", "Digri", "Diplo", "Dokri", "Ghotki", "Haala", "Hyderabad", "Islamkot", "Jacobabad",
-       "Jamshoro", "Jungshahi", "Kandhkot", "Kandiaro", "Karachi", "Kashmore", "Keti Bandar", "Khairpur", "Kotri", "Larkana", "Matiari", "Mehar", "Mirpur Khas", 
-       "Mithani", "Mithi", "Mehrabpur", "Moro", "Nagarparkar", "Naudero", "Naushahro Feroze", "Naushara", "Nawabshah", "Nazimabad", "Qambar", "Qasimabad", "Ranipur", 
-       "Ratodero", "Rohri", "Sakrand", "Sanghar", "Shahbandar", "Shahdadkot", "Shahdadpur", "Shahpur Chakar", "Shikarpaur",
-        "Sukkur", "Tangwani", "Tando Adam", "Tando Allahyar", "Tando Muhammad", "Thatta", "Umerkot", "Warah", "Abbottabad",
-         "Adezai", "Alpuri", "Akora Khattak", "Ayubia", "Banda Daud", "Bannu", "Batkhela", "Battagram", "Birote", "Chakdara", "Charsadda", "Chitral",
-          "Daggar", "Dargai", "Darya Khan", "dera Ismail", "Doaba", "Dir", "Drosh", "Hangu", "Haripur", "Karak", "Kohat", 
-          "Kulachi", "Lakki Marwat", "Latamber", "Madyan", "Mansehra", "Mardan", "Mastuj", "Mingora", "Nowshera", "Paharpur",
-           "Pabbi", "Peshawar", "Saidu Sharif", "Shorkot", "Shewa Adda", "Swabi", "Swat", "Tangi", "Tank", "Thall", 
-           "Timergara", "Tordher", "Awaran", "Barkhan", "Chagai", "Dera Bugti", "Gwadar", "Harnai", "Jafarabad", 
-           "Jhal Magsi", "Kacchi", "Kalat", "Kech", "Kharan", "Khuzdar", "Killa Abdullah", "Killa Saifullah", "Kohlu", 
-           "Lasbela", "Lehri", "Loralai", "Mastung", "Musakhel", "Nasirabad", "Nushki", "Panjgur", "Pishin valley", "Quetta",
-            "Sherani", "Sibi", "Sohbatpur", "Washuk", "Zhob", "Ziarat" ];
+export const cities = ["Islamabad", "Attock", "Bahawalpur", "Faisalabad", "Gujranwala","Karachi", "Lahore", "Multan", "Murree", 
+       "Rawalpindi","Sargodha", "Sialkot", "Hyderabad", "Larkana", "Mirpur Khas","Sukkur",  "Abbottabad", "Nowshera", "Peshawar", "Swat", "Gwadar"];
 
 //console.log(getYears(1990));
